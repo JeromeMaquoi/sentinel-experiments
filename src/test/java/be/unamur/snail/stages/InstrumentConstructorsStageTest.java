2054dfcd3b61928f4224a335db6983175d266cee
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2025-09-25 10:46:24 +0200

    Add one test for InstrumentConstructorStage

---
+++ src/test/java/be/unamur/snail/stages/InstrumentConstructorsStageTest.java
@@ -0,0 +1,46 @@
+package be.unamur.snail.stages;
+
+import be.unamur.snail.config.Config;
+import be.unamur.snail.core.Context;
+import be.unamur.snail.exceptions.MissingContextKeyException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstrumentConstructorsStageTest {
+    private InstrumentConstructorsStage stage;
+    private Context context;
+    private Config config;
+    @TempDir
+    Path tempDir;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        stage = new InstrumentConstructorsStage();
+        context = new Context();
+        Path yaml = tempDir.resolve("config.yaml");
+        Files.writeString(yaml, """
+            project:
+              sub-project: ""
+            repo:
+              url: "https://example.com/repo.git"
+              commit: "123abc"
+              target-dir: "/tmp/repo"
+            log:
+              level: "DEBUG"
+        """);
+        Config.load(yaml.toString());
+        config = Config.getInstance();
+        config.setRepoForTests(new Config.RepoConfig());
+    }
+
+    @Test
+    void shouldThrowExceptionIfClassPathMissingTest() {
+        assertThrows(MissingContextKeyException.class, () -> stage.execute(context));
+    }
+}

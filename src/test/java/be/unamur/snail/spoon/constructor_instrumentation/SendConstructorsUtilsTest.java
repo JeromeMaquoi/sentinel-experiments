b02c0c5affe089fcf6f5d645a3df3e61e4aa3720
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2025-09-25 09:58:18 +0200

    Add tests for getStackTrace

--- src/test/java/be/unamur/snail/spoon/constructor_instrumentation/SendConstructorsUtilsTest.java
+++ src/test/java/be/unamur/snail/spoon/constructor_instrumentation/SendConstructorsUtilsTest.java
@@ -1,20 +1,24 @@
 package be.unamur.snail.spoon.constructor_instrumentation;
 
-import org.apache.commons.lang3.reflect.ConstructorUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class SendConstructorsUtilsTest {
     private SendConstructorsUtils constructorUtils;
 
     @BeforeEach
     void setUp() {
-        constructorUtils = new SendConstructorsUtils();
+        StackTraceHelper mockHelper = Mockito.mock(StackTraceHelper.class);
+        StackTraceElement stackTraceElement = new StackTraceElement("org.springframework.boot.ApplicationEnvironmentTests", "createEnvironment", "ApplicationEnvironmentTests.java", 30);
+        when(mockHelper.getFilteredStackTrace()).thenReturn(List.of(stackTraceElement));
+        constructorUtils = new SendConstructorsUtils(mockHelper);
     }
 
     @Test
@@ -46,5 +50,39 @@ class SendConstructorsUtilsTest {
     @Test
     void addAttributeThrowsExceptionIfConstructorContextNotInitializedTest() {
         assertThrows(IllegalStateException.class, () -> constructorUtils.addAttribute("field", "String", "hello", "literal"));
+    }
+
+    @Test
+    void addAttributeWithNullActualObjectTest() {
+        constructorUtils.initConstructorContext("file.java", "Class", "method", new ArrayList<>(List.of("java.lang.String")));
+        constructorUtils.addAttribute("field", "int", null, "literal");
+
+        ConstructorContext context = constructorUtils.getConstructorContextForTests();
+        assertNotNull(context);
+        assertEquals(1, context.getAttributes().size());
+        AttributeContext attribute = context.getAttributes().iterator().next();
+        assertEquals("field", attribute.getName());
+        assertEquals("int", attribute.getType());
+        assertEquals("null", attribute.getActualType());
+    }
+
+    @Test
+    void getStackTraceThrowsExceptionIfConstructorContextNotInitializedTest() {
+        assertThrows(IllegalStateException.class, () -> constructorUtils.getStackTrace());
+    }
+
+    @Test
+    void getStackTraceWorkingTest() {
+        constructorUtils.initConstructorContext("file.java", "Class", "method", new ArrayList<>(List.of("java.lang.String")));
+        constructorUtils.getStackTrace();
+
+        ConstructorContext context = constructorUtils.getConstructorContextForTests();
+        assertNotNull(context);
+        List<StackTraceElement> stackTrace = context.getStackTrace();
+        assertEquals(1, stackTrace.size());
+        assertEquals("createEnvironment", stackTrace.get(0).getMethodName());
+        assertEquals("ApplicationEnvironmentTests.java", stackTrace.get(0).getFileName());
+        assertEquals("org.springframework.boot.ApplicationEnvironmentTests", stackTrace.get(0).getClassName());
+        assertEquals(30, stackTrace.get(0).getLineNumber());
     }
 }

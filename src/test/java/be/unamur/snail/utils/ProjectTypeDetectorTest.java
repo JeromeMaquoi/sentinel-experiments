5d990d50bb6209f8835595ad320ede3771b02504
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2025-10-23 11:35:17 +0200

    Add tests for ProjectTypeDetector

---
+++ src/test/java/be/unamur/snail/utils/ProjectTypeDetectorTest.java
@@ -0,0 +1,52 @@
+package be.unamur.snail.utils;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectTypeDetectorTest {
+    private File tempFile;
+    private ProjectTypeDetector detector;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        tempFile = Files.createTempDirectory("project").toFile();
+        detector = new ProjectTypeDetector();
+    }
+
+    @AfterEach
+    void tearDown() {
+        deleteRecursively(tempFile);
+    }
+
+    private void deleteRecursively(File file) {
+        if (file.isDirectory()) {
+            for (File child : file.listFiles()) {
+                deleteRecursively(child);
+            }
+        }
+        file.delete();
+    }
+
+    @Test
+    void isGradleProjectTest() throws IOException {
+        File buildGradle = new File(tempFile, "build.gradle");
+        assertTrue(buildGradle.createNewFile());
+
+        assertTrue(detector.isGradleProject(tempFile));
+    }
+
+    @Test
+    void isMavenProjectTest() throws IOException {
+        File buildMaven = new File(tempFile, "pom.xml");
+        assertTrue(buildMaven.createNewFile());
+
+        assertTrue(detector.isMavenProject(tempFile));
+    }
+}

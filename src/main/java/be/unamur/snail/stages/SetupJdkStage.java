a68a1afb0e783baebc1e7a5183c772a65aeea7c5
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2025-11-13 15:42:42 +0100

    Add update-aternatives command handling

--- src/main/java/be/unamur/snail/stages/SetupJdkStage.java
+++ src/main/java/be/unamur/snail/stages/SetupJdkStage.java
@@ -9,6 +9,10 @@ import be.unamur.snail.jdk.SdkmanJdkManager;
 import be.unamur.snail.logging.PipelineLogger;
 import be.unamur.snail.utils.CommandRunner;
 import be.unamur.snail.utils.SimpleCommandRunner;
+import be.unamur.snail.utils.Utils;
+
+import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Stage responsible for detecting, installing and configuring the correct
@@ -54,7 +58,41 @@ public class SetupJdkStage implements Stage {
         log.info("JAVA_HOME set to {}", javaHome);
 
         // Export JAVA_HOME in the shell for subsequent commands
-        String exportCommand = "export JAVA_HOME=" + javaHome;
-        runner.run(exportCommand);
+        runner.run("export JAVA_HOME=" + javaHome);
+        updateAlternatives(log, javaHome);
+    }
+
+    /**
+     * Ensures that update-alternatives recognizes and uses the JDK's java binary.
+     * This method is idempotent and safe to call repeatedly.
+     * @param log The pipeline logger
+     * @param javaHome The JAVA_HOME path of the JDK
+     */
+    public void updateAlternatives(PipelineLogger log, String javaHome) throws IOException, InterruptedException {
+        String javaBinary = javaHome + "/bin/java";
+
+        Utils.CompletedProcess check = runner.run("update-alternatives --query java");
+        boolean alreadyRegistered = Optional.ofNullable(check.stdout())
+                .map(out -> out.contains(javaBinary))
+                .orElse(false);
+        if (!alreadyRegistered) {
+            log.info("Registering {} as an alterative for java", javaBinary);
+            Utils.CompletedProcess install = runner.run(
+                    "sudo update-alternatives --install /usr/bin/java java " + javaBinary + " 1"
+            );
+            if (install.returnCode() != 0) {
+                throw new IOException("Failed to register " + javaBinary + " as an alternative for java: " + install.stderr());
+            }
+        } else {
+            log.info("{} is already registered as an alternative for java", javaBinary);
+        }
+
+        log.info("Setting {} as the active java alternative", javaBinary);
+        Utils.CompletedProcess set = runner.run(
+                "sudo update-alternatives --set java " + javaBinary
+        );
+        if (set.returnCode() != 0) {
+            throw new IOException("Failed to set " + javaBinary + " as the active java alternative: " + set.stderr());
+        }
     }
 }
--- src/test/java/be/unamur/snail/stages/SetupJdkStageTest.java
+++ src/test/java/be/unamur/snail/stages/SetupJdkStageTest.java
@@ -5,7 +5,9 @@ import be.unamur.snail.core.Context;
 import be.unamur.snail.exceptions.MissingConfigKeyException;
 import be.unamur.snail.jdk.JdkManager;
 import be.unamur.snail.logging.ConsolePipelineLogger;
+import be.unamur.snail.logging.PipelineLogger;
 import be.unamur.snail.utils.CommandRunner;
+import be.unamur.snail.utils.Utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
@@ -23,6 +25,7 @@ class SetupJdkStageTest {
     private Context context;
     private CommandRunner runner;
     private SetupJdkStage stage;
+    private PipelineLogger log;
 
     @BeforeEach
     void setUp() {
@@ -32,6 +35,7 @@ class SetupJdkStageTest {
         context = new Context();
         context.setLogger(new ConsolePipelineLogger(SetupJdkStage.class));
         runner = mock(CommandRunner.class);
+        log = mock(PipelineLogger.class);
 
         when(config.getRepo()).thenReturn(repo);
         stage = new SetupJdkStage(manager, config, runner);
@@ -40,9 +44,19 @@ class SetupJdkStageTest {
     @Test
     void executeWithExistingJdkTest() throws Exception {
         String jdkVersion = "17.0.17-tem";
+        String javaHome = "/path/to/jdk17";
+        String javaBinary = javaHome + "/bin/java";
+
         when(repo.getJdk()).thenReturn(jdkVersion);
         when(manager.isInstalled(jdkVersion)).thenReturn(true);
         when(manager.getJavaHome(jdkVersion)).thenReturn("/path/to/jdk17");
+
+        Utils.CompletedProcess ok = new Utils.CompletedProcess("", 0, "", "");
+        Utils.CompletedProcess check = new Utils.CompletedProcess("", 0, javaBinary + "\n", "");
+
+        when(runner.run("update-alternatives --query java")).thenReturn(check);
+        when(runner.run("sudo update-alternatives --set java " + javaBinary)).thenReturn(ok);
+        when(runner.run("export JAVA_HOME=" + javaHome)).thenReturn(ok);
 
         stage.execute(context);
 
@@ -58,9 +72,19 @@ class SetupJdkStageTest {
     @Test
     void executeWithUninstalledJdkTest() throws Exception {
         String jdkVersion = "17.0.17-tem";
+        String javaHome = "/path/to/jdk17";
+        String javaBinary = javaHome + "/bin/java";
+
         when(repo.getJdk()).thenReturn(jdkVersion);
         when(manager.isInstalled(jdkVersion)).thenReturn(false);
         when(manager.getJavaHome(jdkVersion)).thenReturn("/path/to/jdk17");
+
+        Utils.CompletedProcess check = new Utils.CompletedProcess("", 0, "", "");
+        Utils.CompletedProcess ok = new Utils.CompletedProcess("", 0, "", "");
+        when(runner.run("update-alternatives --query java")).thenReturn(check);
+        when(runner.run("sudo update-alternatives --install /usr/bin/java java " + javaBinary + " 1")).thenReturn(ok);
+        when(runner.run("sudo update-alternatives --set java " + javaBinary)).thenReturn(ok);
+        when(runner.run("export JAVA_HOME=" + javaHome)).thenReturn(ok);
 
         stage.execute(context);
 
@@ -91,5 +115,76 @@ class SetupJdkStageTest {
 
         when(manager.getJavaHome(jdkVersion)).thenReturn("");
         assertThrows(Exception.class, () -> stage.execute(context));
+    }
+
+    @Test
+    void updateAlternativesWhenNotRegisteredTest() throws IOException, InterruptedException {
+        String javaHome = "/path/to/jdk17";
+        String javaBinary = javaHome + "/bin/java";
+
+        when(runner.run("update-alternatives --query java"))
+                .thenReturn(new Utils.CompletedProcess("", 0, "Some other java\n", ""));
+        when(runner.run("sudo update-alternatives --install /usr/bin/java java " + javaBinary + " 1"))
+                .thenReturn(new Utils.CompletedProcess("", 0, "", ""));
+        when(runner.run("sudo update-alternatives --set java " + javaBinary))
+                .thenReturn(new Utils.CompletedProcess("", 0, "", ""));
+
+        assertDoesNotThrow(() -> stage.updateAlternatives(log, javaHome));
+
+        verify(log).info(contains("Registering"), anyString());
+        verify(log).info(contains("Setting"), anyString());
+        verify(runner).run(contains("--install"));
+        verify(runner).run(contains("--set"));
+    }
+
+    @Test
+    void updateAlternativesWhenAlreadyRegisteredTest() throws IOException, InterruptedException {
+        String javaHome = "/path/to/jdk17";
+        String javaBinary = javaHome + "/bin/java";
+
+        when(runner.run("update-alternatives --query java"))
+                .thenReturn(new Utils.CompletedProcess("", 0, "Some line\n" + javaBinary + "\n", ""));
+        when(runner.run("sudo update-alternatives --set java " + javaBinary))
+                .thenReturn(new Utils.CompletedProcess("", 0, "", ""));
+
+        assertDoesNotThrow(() -> stage.updateAlternatives(log, javaHome));
+
+        verify(log).info(contains("already registered"), anyString());
+        verify(log).info(contains("Setting"), anyString());
+        verify(runner, never()).run(contains("--install"));
+        verify(runner).run(contains("--set"));
+    }
+
+    @Test
+    void updateAlternativesWhenInstallFailsShouldThrowExceptionTest() throws IOException, InterruptedException {
+        String javaHome = "path/to/jdk17";
+        String javaBinary = javaHome + "/bin/java";
+
+        when(runner.run("update-alternatives --query java"))
+                .thenReturn(new Utils.CompletedProcess("", 0, "Some other java\n", ""));
+        when(runner.run("sudo update-alternatives --install /usr/bin/java java " + javaBinary + " 1"))
+                .thenReturn(new Utils.CompletedProcess("", 1, "", "Installation error"));
+
+        IOException exception = assertThrows(IOException.class, () -> stage.updateAlternatives(log, javaHome));
+        assertTrue(exception.getMessage().contains("Failed to register"));
+
+        verify(runner).run(contains("--install"));
+        verify(runner, never()).run(contains("--set"));
+    }
+
+    @Test
+    void updateAlterativesWhenSetFailsShouldThrowExceptionTest() throws IOException, InterruptedException {
+        String javaHome = "path/to/jdk17";
+        String javaBinary = javaHome + "/bin/java";
+
+        when(runner.run("update-alternatives --query java"))
+                .thenReturn(new Utils.CompletedProcess("", 0, javaBinary + "\n", ""));
+        when(runner.run("sudo update-alternatives --set java " + javaBinary))
+                .thenReturn(new Utils.CompletedProcess("", 1, "", "Set error"));
+
+        IOException exception = assertThrows(IOException.class, () -> stage.updateAlternatives(log, javaHome));
+        assertTrue(exception.getMessage().contains("Failed to set"));
+
+        verify(runner).run(contains("--set"));
     }
 }

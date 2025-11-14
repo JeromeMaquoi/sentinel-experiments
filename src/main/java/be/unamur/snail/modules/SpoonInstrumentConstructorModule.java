c7fe6532f3f5b9a6d6dd8abf7be18ef410424b5b
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2025-11-14 16:10:11 +0100

    Add stop backend stage in instrument module

--- src/main/java/be/unamur/snail/modules/SpoonInstrumentConstructorModule.java
+++ src/main/java/be/unamur/snail/modules/SpoonInstrumentConstructorModule.java
@@ -23,6 +23,7 @@ public class SpoonInstrumentConstructorModule implements Module {
         BackendServiceManagerFactory backendFactory = new SimpleBackendServiceManagerFactoryImpl();
         DatabasePreparerFactory databaseFactory = new SimpleDatabasePreparerFactory(mongo);
         this.stages = List.of(
+                new StopBackendStage(runner, backendFactory, databaseFactory),
                 new PrepareBackendStage(runner, backendFactory, databaseFactory),
                 new CloneAndCheckoutRepositoryStage(),
                 //new CopyDirectoryStage(),

14da034ed8d478a68ca44bd8adc1cb62ffe76496
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2026-02-12 12:24:47 +0100

    Beginning of docs for the config files

--- src/main/java/be/unamur/snail/core/Config.java
+++ src/main/java/be/unamur/snail/core/Config.java
@@ -8,19 +8,38 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.util.List;
 
+/**
+ * Config class represents the configuration of the application, which is loaded from a YAML file. It contains nested static classes to represent different sections of the configuration, such as project details, repository information, logging settings, execution plan, and backend configuration. The Config class follows the singleton pattern to ensure that the configuration is loaded only once and can be accessed globally throughout the application.
+ */
 public class Config {
+    /**
+     * Singleton instance of the configuration. It is loaded once at the start of the application and can be accessed globally via getInstance().
+     */
     private static Config instance;
+    /**
+     * Configuration for the project being analyzed, including its name, owner, subproject (if applicable), whether to show project logs, and package prefix for instrumentation.
+     */
     private ProjectConfig project;
     private RepoConfig repo;
     private LogConfig log;
+    /**
+     * Path of the code to add to the project being analyzed. This code will be executed during the test suite execution of the instrumented project and is used to retrieve constructor data during this execution.
+     */
     @JsonProperty("code-constructors-instrumentation-path")
     private String codeConstructorsInstrumentationPath;
+    /**
+     * Timeout in seconds for executing commands during the pipeline execution. This is used to prevent hanging processes and ensure that the pipeline can recover from failures in a timely manner. The default value is set to 120 seconds, but it can be overridden in the configuration file if needed.
+     */
     @JsonProperty("command-time-out")
     private int commandTimeout = 120;
     @JsonProperty("execution-plan")
     private ExecutionPlanConfig executionPlan;
     private BackendConfig backend;
 
+    /**
+     * Returns the singleton instance of the configuration. If the configuration has not been loaded yet, it throws a ConfigNotLoadedException.
+     * @return the singleton instance of the configuration
+     */
     public static Config getInstance() {
         if (instance == null) {
             throw new ConfigNotLoadedException();
@@ -94,17 +113,37 @@ public class Config {
     }
 
 
-
+    /**
+     * ProjectConfig class represents the configuration related to the project being analyzed. It is a nested static class within the Config class and is used to encapsulate all project-related configuration details.
+     */
     public static class ProjectConfig {
+        /**
+         * The name of the project being analyzed. This is a required field in the configuration and is used to identify the project in logs, during the instrumentation process and the energy measurement process. It is also used to determine the location of build files and other resources related to the project.
+         */
         private String name;
+        /**
+         * The GitHub owner of the project.
+         */
         private String owner;
+        /**
+         * The subproject or module within the main project that is being analyzed. This is an optional field and can be used to specify a particular submodule of a larger project, allowing for more targeted instrumentation and analysis. If not specified, the entire project will be analyzed.
+         */
         @JsonProperty("sub-project")
         private String subProject;
+        /**
+         * A boolean flag indicating whether to show project logs during the execution of the pipeline.
+         */
         @JsonProperty("show-project-logs")
         private boolean showProjectLogs;
+        /**
+         * An optional package prefix used to filter stacktraces during the instrumentation process.
+         */
         @JsonProperty("package-prefix")
         private String packagePrefix;
 
+        /**
+         * Returns the name of the project being analyzed.
+         */
         public String getName() {
             return name;
         }
@@ -113,6 +152,9 @@ public class Config {
             this.name = name;
         }
 
+        /**
+         * Returns the GitHub owner of the project.
+         */
         public String getOwner() {
             return owner;
         }
@@ -121,6 +163,9 @@ public class Config {
             this.owner = owner;
         }
 
+        /**
+         * Returns, if it exists, the subproject or module within the project that is being analyzed. This is used to specify a particular submodule of a larger project, allowing for more targeted instrumentation and analysis. If not specified, the entire project will be analyzed.
+         */
         public String getSubProject() {
             return subProject;
         }
@@ -129,10 +174,16 @@ public class Config {
             this.subProject = subProject;
         }
 
+        /**
+         * Returns true if project logs should be shown during the execution of the pipeline, false otherwise.
+         */
         public boolean isShowProjectLogs() {
             return showProjectLogs;
         }
 
+        /**
+         * Returns the package prefix used to filter stacktraces during the instrumentation process.
+         */
         public String getPackagePrefix() {
             return packagePrefix;
         }
@@ -152,10 +203,16 @@ public class Config {
         @JsonProperty("overwrite-copy")
         private boolean overwriteCopy;
 
+        /**
+         * Returns the URL of the Git repository of the project being analyzed.
+         */
         public String getUrl() {
             return url;
         }
 
+        /**
+         * Returns the specific commit hash that should be checked out in the repository, for the analysis.
+         */
         public String getCommit() {
             return commit;
         }
@@ -164,6 +221,9 @@ public class Config {
             this.commit = commit;
         }
 
+        /**
+         * Returns the target directory where the repository should be cloned.
+         */
         public String getTargetDir() {
             return targetDir;
         }
@@ -172,6 +232,9 @@ public class Config {
             this.targetDir = targetDir;
         }
 
+        /**
+         * Returns the JDK version that is used by the project being analyzed, so that this version is used during the instrumentation and the test execution process.
+         */
         public String getJdk() {
             return jdk;
         }
@@ -180,6 +243,9 @@ public class Config {
             this.jdk = jdk;
         }
 
+        /**
+         * Returns true if the repository should be re-cloned even if it already exists in the target directory, false otherwise.
+         */
         public boolean isOverwriteClone() {
             return overwriteClone;
         }
@@ -188,6 +254,9 @@ public class Config {
             this.overwriteClone = overwrite;
         }
 
+        /**
+         * Returns true if the copy of the cloned repository should be overwritten or not.
+         */
         public boolean isOverwriteCopy() {
             return overwriteCopy;
         }

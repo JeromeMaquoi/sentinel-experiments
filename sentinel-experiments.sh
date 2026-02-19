e5d90bf61c435e9e6ef010812c43c849099dfcd6
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2026-02-19 17:33:31 +0100

    Update of sentinel-experiment jar path

--- sentinel-experiments.sh
+++ sentinel-experiments.sh
@@ -3,4 +3,4 @@ export JAVA_HOME=/usr/lib/jvm/java-19-openjdk-amd64/
 export PATH=$JAVA_HOME/bin:$PATH
 
 mvn clean package
-java -cp /home/jerome/Documents/Assistant/Recherche/joular-scripts/sentinel-experiments/target/sentinel-experiments-1.0-SNAPSHOT-jar-with-dependencies.jar be.unamur.snail.Main "$@"
+java -cp /home/commun/infom125/sentinel-group/sentinel-experiments/target/sentinel-experiments-1.0-SNAPSHOT-jar-with-dependencies.jar be.unamur.snail.Main "$@"
